601cc31aba7203943e0d44d2d87a42b6c2a9b6cd
hhakangull <hakanngul@icloud.com>
2023-03-17 20:38:34 +0300

    Initial Commit

---
+++ SwiftUi-with-Firebase/SwiftUi_with_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_with_FirebaseApp.swift
+//  SwiftUi-with-Firebase
+//
+//  Created by Hakan Gül on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_with_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
